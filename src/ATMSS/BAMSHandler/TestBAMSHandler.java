99af34f56f7e07473b4495d3c4e4c48325780479
SanzharNussipbek <nsanzhar.99@gmail.com>
2021-04-05 21:48:32 +0600

    feat: use own url for back-end in TestBAMSHandler

--- src/ATMSS/BAMSHandler/TestBAMSHandler.java
+++ src/ATMSS/BAMSHandler/TestBAMSHandler.java
@@ -13,7 +13,7 @@ public class TestBAMSHandler {
     //------------------------------------------------------------
     // main
     public static void main(String[] args) {
-        String urlPrefix = "http://cslinux0.comp.hkbu.edu.hk/~comp4107/test/";
+        String urlPrefix = "http://cslinux0.comp.hkbu.edu.hk/comp4107_20-21_grp04/";
 
         // try logging in
         // BAMSHandler bams = new BAMSHandler(urlPrefix);		// without logger
